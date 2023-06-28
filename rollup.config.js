a346c64baa3f7fa6888e2aeeb33b7b00e9bc7b6d
hojae-lee <chosloo2835@gmail.com>
2023-06-28 11:31:25 +0900

    add : esm, cjs 빌드 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,25 @@
 import esbuild from "rollup-plugin-esbuild";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import pkg from "./package.json" assert { type: "json" };
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "bundle.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: pkg.exports["."].import,
+      format: "esm",
+    },
+    {
+      file: pkg.exports["."].require,
+      format: "cjs",
+    },
+  ],
   plugins: [
     esbuild({
       // All options are optional
       include: /\.[jt]sx?$/, // default, inferred from `loaders` option
       minify: true,
       jsx: "automatic", // default, or 'preserve'
-      // Like @rollup/plugin-replace
-      define: {
-        __VERSION__: '"x.y.z"',
-      },
     }),
     peerDepsExternal(),
   ],
